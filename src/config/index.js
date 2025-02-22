733149ce703d6dc9a6ffe2e4735cfe160ff4b780
baobao <minhbao1223@gmail.com>
2025-02-22 14:57:20 +0700

    Your commit message here

--- src/config/index.js
+++
@@ -1,5 +0,0 @@
-export default {
-    serverURL : "http://samples-mta.herokuapp.com/shoppingCart",
-    CLOUDINARY_UPLOAD_PRESET : "lfzyzanj",
-    CLOUDINARY_UPLOAD_URL : "https://api.cloudinary.com/v1_1/ibnferoz/image/upload"
-}
--- src/containers/adminOrders/renderer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function () {
-    return (
-        <div>
-            <h1> Summary of ordered Products </h1>
-        </div>
-    )
-}
